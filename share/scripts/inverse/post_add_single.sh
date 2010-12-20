ac13625345dfb879d2bd29bee1e249df06a9f442
Christoph Junghans <junghans@mpip-mainz.mpg.de>
2010-12-20 12:00:49 +0100

    integrate.pl -> table_integrate.pl

---
+++ share/scripts/inverse/post_add_single.sh
@@ -0,0 +1,46 @@
+#! /bin/bash
+#
+# Copyright 2009 The VOTCA Development Team (http://www.votca.org)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+if [ "$1" = "--help" ]; then
+cat <<EOF
+${0##*/}, version %version%
+This script make all the post update with backup for single pairs
+
+Usage: ${0##*/}
+
+USES:  csg_get_interaction_property mv do_external successful_or_die
+
+NEEDS: name inverse.post_add
+EOF
+   exit 0
+fi
+
+name=$(csg_get_interaction_property name)
+tasklist=$(csg_get_interaction_property --allow-empty inverse.post_add)
+i=1
+#after all we shift the potential to be 0 at the cutoff and tag with labels
+for task in $tasklist shift tag; do
+  echo "Doing postadd task '$task' for '${name}'"
+  
+  #save the current one
+  successful_or_die mv "${name}.pot.new" "${name}.pot.${i}"
+
+  #perform postadd task
+  do_external postadd "$task" "${name}.pot.${i}" "${name}.pot.new" 
+
+  ((i++))
+done
---
+++ share/scripts/inverse/postadd_convergence.sh
@@ -0,0 +1,80 @@
+#! /bin/bash
+#
+# Copyright 2009 The VOTCA Development Team (http://www.votca.org)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+if [ "$1" = "--help" ]; then
+cat <<EOF
+${0##*/}, version %version%
+postadd convergence script, calcs int of (\${name}.DIST.tgt-\${name}.DIST.new)**2
+and save it to \${name}.conv. DIST ist dist, but changed by onvergence.what option
+
+usage: ${0##*/} infile outfile
+
+USES: die check_deps do_external wc sed awk paste mktemp successful_or_die
+
+NEEDS: name
+
+OPTIONAL: inverse.post_add_options.convergence.weight inverse.post_add_options.convergence.what
+EOF
+   exit 0
+fi
+
+check_deps "$0"
+
+[ -z "$2" ] && die "${0##*/}: Missing arguments"
+
+do_external postadd dummy "$1" "$2"
+
+name=$(csg_get_interaction_property name)
+step=$(csg_get_interaction_property step)
+
+#these two are arrays
+weights=( $(csg_get_interaction_property inverse.post_add_options.convergence.weight 1) )
+what_to_do_list=( $(csg_get_interaction_property inverse.post_add_options.convergence.what "dist") )
+
+[ ${#weights[@]} -ne ${#what_to_do_list[@]} ] && die "${0##*/}: number of weights does not match number of 'what' to calc convergence from"
+tmp="$(successful_or_die mktemp ${name}.conv.XXX)"
+
+#we allow multiple thing per interaction to be checked
+for ((i=0;i<${#what_to_do_list[@]};i++)); do
+  dist=${what_to_do_list[$i]}
+  weight=${weights[$i]}
+  tmp1="$(successful_or_die mktemp ${name}.${dist}.tgt.XXX)"
+  tmp2="$(successful_or_die mktemp ${name}.${dist}.new.XXX)"
+  tmp3="$(successful_or_die mktemp ${name}.${dist}.cmb.XXX)"
+
+  if [ ! -f "${name}.${dist}.tgt" ]; then
+    #if we need $name.dist.tgt we know how to create it
+    if [ "${dist}" = "dist" ]; then
+      do_external resample target
+    else
+      die "${0##*/}: file '${name}.${dist}.tgt' was not found, add the script to create this file to the postadd routine of interaction $name"
+    fi
+  fi
+
+  successful_or_die sed -e '/^#/d' -e 's/nan/0.0/g' ${name}.${dist}.tgt > $tmp1
+  successful_or_die sed -e '/^#/d' -e 's/nan/0.0/g' ${name}.${dist}.new > $tmp2
+
+  [ $(sed -n '$=' $tmp1) -eq $(sed -n '$=' $tmp2) ] || \
+    die "${0##*/}: linenumber of ${name}.${dist}.tgt differs from ${name}.${dist}.new"
+
+  successful_or_die paste $tmp1 $tmp2 > $tmp3
+  successful_or_die awk '{if ($4!=$1){print "differ in line NR";exit 1;}}' $tmp3
+  echo "Calc convergence for ${name} with weight $weight"
+  successful_or_die awk -v bin=$step -v w=$weight -v dist=$dist '{sum+=($5-$2)**2;}END{print dist,sqrt(sum*bin*w);}' $tmp3 >> $tmp
+done
+
+successful_or_die awk '{sum+=$2;}END{print sum;}' $tmp > ${name}.conv
