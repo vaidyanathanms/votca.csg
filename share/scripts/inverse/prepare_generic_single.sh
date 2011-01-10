d90a82906ddb225252dc99c614403bef51f2cab1
Christoph Junghans <junghans@mpip-mainz.mpg.de>
2011-01-10 17:51:11 +0100

    merge with Georg

---
+++ share/scripts/inverse/prepare_generic_single.sh
@@ -0,0 +1,58 @@
+#! /bin/bash
+#
+# Copyright 2009 The VOTCA Development Team (http://www.votca.org)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+if [ "$1" = "--help" ]; then
+cat <<EOF
+${0##*/}, version %version%
+This script implemtents the function initialize
+for the Inverse Boltzmann Method
+
+Usage: ${0##*/}
+
+USES: do_external csg_get_interaction_property critical csg_resample check_deps msg get_main_dir mktemp
+
+NEEDS: name min max step
+
+OPTIONAL: inverse.target
+EOF
+  exit 0
+fi
+
+check_deps "$0"
+
+name=$(csg_get_interaction_property name)
+min=$(csg_get_interaction_property min )
+max=$(csg_get_interaction_property max )
+step=$(csg_get_interaction_property step )
+comment="$(get_table_comment)"
+main_dir=$(get_main_dir)
+
+if [ -f "${main_dir}/${name}.pot.in" ]; then
+  msg "Using given table ${name}.pot.in for ${name}"
+  tmp="$(critical mktemp ${name}.pot.in.smooth.XXX)"
+  echo "Converting ${main_dir}/${name}.pot.in to ${name}.pot.new through $tmp"
+  critical csg_resample --in "${main_dir}/${name}.pot.in" --out ${tmp} --grid ${min}:${step}:${max} --comment "$comment"
+  do_external pot shift_nb ${tmp} ${name}.pot.new
+else
+  target=$(csg_get_interaction_property inverse.target)
+  msg "Using initial guess from dist ${target} for ${name}"
+  #copy+resample all target dist in $this_dir
+  critical csg_resample --in ${main_dir}/${target} --out ${name}.dist.tgt --grid ${min}:${step}:${max} --comment "${comment}"
+  # RDF_to_POT.pl just does log g(r) + extrapolation
+  do_external rdf pot ${name}.dist.tgt ${name}.pot.new
+fi
+
---
+++ share/scripts/inverse/solve_matlab.sh
@@ -0,0 +1,47 @@
+#! /bin/bash
+#
+# Copyright 2009 The VOTCA Development Team (http://www.votca.org)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+if [ "$1" = "--help" ]; then
+cat <<EOF
+${0##*/}, version %version%
+This solves linear equation system from imc using matlab
+
+Usage: ${0##*/} <group> <outfile>
+
+USES:  die sed matlab rm critical cat_external mv check_deps
+
+NEEDS:
+EOF
+   exit 0
+fi
+
+check_deps "$0"
+
+[[ -n "$2" ]] || die "${0##*/}: Missing arguments"
+
+# initialize & run the matlab file
+cat_external solve matlab | sed -e "s/\$name_out/$2/" -e "s/\$name/$1/" > solve_$1.m || die "${0##*/}: sed failed"
+
+#matlab does not like -_. etc in filenames
+critical mv solve_$1.m solve.m
+critical matlab -r solve -nosplash -nodesktop
+rm -f solve.m
+
+# temporary compatibility issue
+[[ -f "$2" ]] || die "Matlab failed"
+critical sed -ie 's/NaN/0.0/' $2
+critical sed -ie 's/Inf/0.0/' $2
