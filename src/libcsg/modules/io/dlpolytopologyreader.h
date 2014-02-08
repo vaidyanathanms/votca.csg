5988eabe16b3ffa8a4a7bd6d27a645c3a323880d
Christoph Junghans <christoph.junghans@gmail.com>
2014-02-07 19:12:05 -0700

    potentials_to_dlpoly.sh: fixed adding of header

---
+++ src/libcsg/modules/io/dlpolytopologyreader.h
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2009-2013 The VOTCA Development Team (http://www.votca.org)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#ifndef _DLPTOPOLOGYREADER_H
+#define	_DLPTOPOLOGYREADER_H
+
+#include <string>
+#include <votca/csg/topology.h>
+#include <votca/csg/topologyreader.h>
+
+namespace votca { namespace csg {
+using namespace votca::tools;
+
+using namespace std;
+
+/**
+    \brief reader for dlpoly FIELD (topology) files
+
+    This class encapsulates the dlpoly reading functions and provides an interface to fill a topolgy class
+
+*/
+class DLPOLYTopologyReader
+   : public TopologyReader
+{
+public:
+    DLPOLYTopologyReader() {}
+
+    /// read a topology file
+    bool ReadTopology(string file, Topology &top);
+  
+    void   setFname(string name) { _fname = name; return; }
+    string getFname()            { return _fname; }
+
+private:
+    string _fname;
+    string _NextKeyline(ifstream &fs, const char* wsp);
+    string _NextKeyInt(ifstream &fs, const char* wsp, const string &word, int &ival);
+    bool   _isKeyInt(const string &line, const char* wsp, const string &word, int &ival);
+};
+
+}}
+
+#endif	/* _DLPTOPOLOGYREADER_H */
